5a54917eec2f6d5e0b1c4298e4346e3fc2b0c3a3
wyyoon <wonyoung5488@hanmail.net>
2020-05-15 18:21:04 +0900

    인프런 스프링 웹 MVC 스프링 MVC 소개 예제

---
+++ src/main/java/me/whiteship/demospringmvc/Event.java
@@ -0,0 +1,17 @@
+package me.whiteship.demospringmvc;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Event {
+    private String name;
+    private int limitOfEnrollment;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
+}
---
+++ src/main/java/me/whiteship/demospringmvc/EventService.java
@@ -0,0 +1,28 @@
+package me.whiteship.demospringmvc;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class EventService {
+    public List<Event> getEvents(){
+        Event event = Event.builder()
+                .name("스프링 웹 MVC 스터디 1차")
+                .limitOfEnrollment(5)
+                .startDateTime(LocalDateTime.of(2019,1,10,10,0))
+                .endDateTime(LocalDateTime.of(2019,1,10,12,0))
+                .build();
+
+        Event event2 = Event.builder()
+                .name("스프링 웹 MVC 스터디 2차")
+                .limitOfEnrollment(5)
+                .startDateTime(LocalDateTime.of(2019,1,17,10,0))
+                .endDateTime(LocalDateTime.of(2019,1,17,12,0))
+                .build();
+
+        return Arrays.asList(event, event2);
+    }
+}
