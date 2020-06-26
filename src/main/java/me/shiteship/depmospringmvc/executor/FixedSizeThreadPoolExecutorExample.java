6f0639c4e2fb7ac55d138eff43392c3ca8e92bf8
wyyoon <wonyoung5488@hanmail.net>
2020-06-26 15:16:49 +0900

    FixedThreadPool 모든 task 완료 될때 까지...

--- src/main/java/me/shiteship/depmospringmvc/executor/FixedSizeThreadPoolExecutorExample.java
+++ src/main/java/me/shiteship/depmospringmvc/executor/FixedSizeThreadPoolExecutorExample.java
@@ -2,10 +2,11 @@ package me.shiteship.depmospringmvc.executor;
 
 import java.time.LocalTime;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 public class FixedSizeThreadPoolExecutorExample {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws InterruptedException {
 
         ThreadPoolExecutor executor = (ThreadPoolExecutor) Executor.newFixedThreadPool(4);
         for(int i=0; i<10; i++){
@@ -14,10 +15,27 @@ public class FixedSizeThreadPoolExecutorExample {
 
             executor.execute(() -> {
                 System.out.println(LocalTime.now() + " Doing job "+jobId);
+                sleepSec(3);
                 System.out.println(LocalTime.now() + " Done job "+jobId);
             });
         }
 
         executor.shutdown();
+
+        if(executor.awaitTermination(20, TimeUnit.SECONDS)){
+            System.out.println(LocalTime.now() + " All jobs are terminated ");
+        } else {
+            System.out.println(LocalTime.now() + " some jobs are not terminated ");
+
+            executor.shutdown();
+        }
+    }
+
+    private static void sleepSec(int sec){
+        try {
+            TimeUnit.SECONDS.sleep(sec);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
