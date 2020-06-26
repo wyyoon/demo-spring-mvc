0759b8c86a26a1d5e438b4362e4f627fce428af5
wyyoon <wonyoung5488@hanmail.net>
2020-06-26 16:44:04 +0900

    FixedThreadPool BlockingQueue로 결과 리턴 받기

--- src/main/java/me/shiteship/depmospringmvc/executor/FixedSizeThreadPoolExecutorExample.java
+++ src/main/java/me/shiteship/depmospringmvc/executor/FixedSizeThreadPoolExecutorExample.java
@@ -1,24 +1,41 @@
 package me.shiteship.depmospringmvc.executor;
 
+import org.apache.tomcat.jni.Local;
+
 import java.time.LocalTime;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class FixedSizeThreadPoolExecutorExample {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        final int maxCore = Runtime.getRuntime().availableProcessors();
-        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executor.newFixedThreadPool(maxCore);
-        System.out.println("Max thread pool size : " + executor.getMaximumPoolSize());
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executor.newFixedThreadPool(4);
+        final BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
 
         for(int i=0; i<10; i++){
             final int jobId = i;
-            System.out.println(" Execute tase " + jobId);
+            final int sleepSec = 10 - i;
+            System.out.println(LocalTime.now() + " Execute task " + jobId);
 
-            executor.execute(() -> {
-                System.out.println(" Doing job "+jobId);
+            executor.submit(() -> {
+                System.out.println(LocalTime.now() + " Doing Task " + jobId + ", sleepSec : " + sleepSec);
+
+                try {
+                    TimeUnit.SECONDS.sleep(sleepSec);
+                    String result = "finished job " + jobId;
+                    queue.put(result);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             });
+
+        }
+
+        for(int i = 0; i<10; i++){
+            String result = queue.take();
+            System.out.println(LocalTime.now()+" "+ result);
         }
 
         executor.shutdown();
